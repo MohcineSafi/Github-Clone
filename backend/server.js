93244a6fe1790c6e55a2cad7efcf73194400f27f
MohcineSafi <safi.mouhcine02@gmail.com>
2024-04-28 00:15:35 +0100

    Feat:Configured server to listen on port 5000 and set up routes for exploring popular repositories

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import exploreRoutes from "./routes/explore.route.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.use("/api/explore", exploreRoutes);
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
