c07bcc6ebcacdba09bd42c88073aacba5c6457d1
MohcineSafi <safi.mouhcine02@gmail.com>
2024-04-29 00:28:05 +0100

    Feat:Updated server file with user route and connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
+import userRoutes from "./routes/user.route.js";
 import exploreRoutes from "./routes/explore.route.js";
+import connectMongoDB from "./db/connectMongoDB.js"; // Import the connectMongoDB function
 
 dotenv.config();
 
@@ -13,8 +15,11 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
+app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
-app.listen(5000, () => {
+app.listen(5000, async () => {
+  // Make the callback function async to use await
   console.log("Server is running on port 5000");
+  await connectMongoDB(); // Call the connectMongoDB function
 });
