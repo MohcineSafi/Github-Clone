488eba0bde3ea3ec6c3f37f666b44d22d28a3288
MohcineSafi <safi.mouhcine02@gmail.com>
2024-05-01 00:17:22 +0100

    Feat:Initialize Passport and session middleware

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import passport from "passport";
+import session from "express-session";
+
+import "./passport/github.auth.js";
 
 import userRoutes from "./routes/user.route.js";
 import exploreRoutes from "./routes/explore.route.js";
 import authRoutes from "./routes/auth.route.js";
 
-import connectMongoDB from "./db/connectMongoDB.js"; // Import the connectMongoDB function
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
-const app = express();
+const app = express(); // Only one declaration of 'app'
+
+app.use(
+  session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -22,7 +34,6 @@ app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
 app.listen(5000, async () => {
-  // Make the callback function async to use await
   console.log("Server is running on port 5000");
-  await connectMongoDB(); // Call the connectMongoDB function
+  await connectMongoDB();
 });
