337dbad145c84d2a5e2898e42b8e1b6244a37a85
MohcineSafi <safi.mouhcine02@gmail.com>
2024-04-30 17:17:09 +0100

    Feat:Add route to check authentication status and return user information if authenticated

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -8,4 +8,22 @@ router.get(
   passport.authenticate("github", { scope: ["user:email"] })
 );
 
+router.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: process.env.CLIENT_BASE_URL + "/login",
+  }),
+  function (req, res) {
+    res.redirect(process.env.CLIENT_BASE_URL);
+  }
+);
+
+router.get("/check", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.send({ user: req.user });
+  } else {
+    res.send({ user: null });
+  }
+});
+
 export default router;
