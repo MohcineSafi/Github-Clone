9b6a29f644b723a1587af3238d39d6ab62c7a582
MohcineSafi <safi.mouhcine02@gmail.com>
2024-05-03 12:22:58 +0100

    Feat:Updated server configuration to optimize session handling and removed unnecessary CORS middleware for improved production performance.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import passport from "passport";
 import session from "express-session";
+import path from "path";
 
 import "./passport/github.auth.js";
 
@@ -14,26 +15,32 @@ import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
-const app = express(); // Only one declaration of 'app'
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
 );
-
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+// Here we can remove the cors, it's not necessary in production because the frontend and backend are on the same domain. I forgot to mention that in the video, sorry about that.🙄
+// app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
-app.listen(5000, async () => {
-  console.log("Server is running on port 5000");
-  await connectMongoDB();
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+  connectMongoDB();
 });
